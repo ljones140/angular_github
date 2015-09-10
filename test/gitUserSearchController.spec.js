51245cc19978297f8425de32f28b97335b183ce0
Benjamina Bollag <benjamina.bollag12@imperial.ac.uk>
2015-09-10 10:45:30 +0100

    controller tests independent of factory

--- test/gitUserSearchController.spec.js
+++ test/gitUserSearchController.spec.js
@@ -1,54 +1,54 @@
 describe('GitUserSearchController', function() {
-
-  beforeEach(module('GitUserSearch'));
-
+  beforeEach(module('GitUserSearch')); // creating a mock module
   var ctrl;
+  
+  describe('when user searching for a user', function() {
+    var fakeUserInfo, scope, fakeSearch, fakeCounter;
 
-  beforeEach(inject(function($controller) {
-    ctrl = $controller('GitUserSearchController');
-  }));
-
-  it('initialises with an empty search result and term', function() {
-    expect(ctrl.searchResult).toBeUndefined();
-    expect(ctrl.searchTerm).toBeUndefined();
-  });
-
-  describe('when searching for a user', function() {
-
-    var httpBackend;
-    beforeEach(inject(function($httpBackend) {
-      httpBackend = $httpBackend
-      httpBackend
-      .expectGET("https://api.github.com/search/users?access_token="
-        + githubToken +'&q=hello')
-      .respond(
-        { items: items }
-        );
-    }));
-
-    afterEach(function() {
-      httpBackend.verifyNoOutstandingExpectation();
-      httpBackend.verifyNoOutstandingRequest();
-   });
-
-    var items = [
-      {
-        "login": "tansaku",
-        "avatar_url": "https://avatars.githubusercontent.com/u/30216?v=3",
-        "html_url": "https://github.com/tansaku"
-      },
-      {
-        "login": "stephenlloyd",
-        "avatar_url": "https://avatars.githubusercontent.com/u/196474?v=3",
-        "html_url": "https://github.com/stephenlloyd"
-      }
-    ];
-
-    it('displays search results', function() {
-      ctrl.searchTerm = 'hello';
-      ctrl.doSearch();
-      httpBackend.flush();
-      expect(ctrl.searchResult.items).toEqual(items);
+    beforeEach(function(){
+      fakeSearch  = jasmine.createSpyObj('fakeSearch',  ['query']);
+      fakeCounter = jasmine.createSpyObj('fakeCounter', ['query']) ;
+      
+      module({
+        Search:      fakeSearch,
+        RepoCounter: fakeCounter
+      });
     });
+   
+    var gitHubSearchResponse = {
+      "items" : [
+        {
+          "login"     : "tansaku",
+          "avatar_url": "https://avatars.githubusercontent.com/u/30216?v=3",
+          "html_url"  : "https://github.com/tansaku"
+        }
+      ]
+    }
+
+    var mockUserData = JSON.parse(JSON.stringify(gitHubSearchResponse)); 
+    
+    beforeEach(inject(function ($q, $rootScope, $controller) {
+      scope = $rootScope;
+      fakeSearch.query.and.returnValue($q.when( { data: gitHubSearchResponse }));
+      fakeCounter.query.and.returnValue($q.when({ data: '20' }));
+      ctrl = $controller('GitUserSearchController');
+    }));
+    
+    it("includes user search results in user data", function() {
+      ctrl.searchTerm = 'tansaku';
+      ctrl.doSearch();
+      scope.$apply();
+      expect(ctrl.searchResult.items).toEqual(gitHubSearchResponse.items);
+    });
+
+    // it("includes user repo count in user data", function() {
+    //   mockUserData.items[0].repoCount = '20';
+    //   ctrl.searchTerm = 'tansaku';
+    //   ctrl.doSearch();
+    //   scope.$apply();
+    //   expect(ctrl.searchResult.items).toEqual(mockUserData.items);
+    // });
   });
+
+  
 });
