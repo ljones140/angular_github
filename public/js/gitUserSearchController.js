437ecc04a51e80d876dd2e050cfc5b59e105db41
lewis <ljones140@gmail.com>
2015-09-10 15:30:23 +0100

    sort capital G

---
+++ public/js/gitUserSearchController.js
@@ -0,0 +1,14 @@
+githubUserSearch.controller('GitUserSearchController', ['Search', function(Search) {
+
+	var self = this;
+
+	self.doSearch = function() {
+	  if (self.searchTerm) {
+	    Search.query(self.searchTerm)
+	      .then(function(response) {
+	      self.searchResult = response.data;
+	      });
+	  }
+	};
+}]);
+
