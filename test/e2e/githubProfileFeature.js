85aec38083b2682704117839573e0d552d0187c2
lewis <ljones140@gmail.com>
2015-09-10 11:41:10 +0100

    feature testing added

--- test/e2e/githubProfileFeature.js
+++ test/e2e/githubProfileFeature.js
@@ -20,7 +20,8 @@ describe('Github Profile finder', function() {
     searchBox.sendKeys('spike01');
     searchButton.click();
 
-    expect(element(by.binding('user.login')).getText()).toEqual('spike01');
+    var profiles = element.all(by.repeater('user in searchCtrl.searchResult.items'));
+    expect(profiles.last().getText()).toEqual('spike01');
 
   });
 });
